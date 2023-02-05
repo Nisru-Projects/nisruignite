c1b9b9d15b65c9c6376bc285b09204afe9331752
Eman134 <kaykeeman@gmail.com>
2023-02-05 17:52:38 -0300

    Test react in adonis

--- app/Controllers/Http/AccountController.ts
+++ app/Controllers/Http/AccountController.ts
@@ -1,25 +1,25 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class AccountController {
-  public async create({ response, session }) {
-    if (!session.get('account')) {
-      Database.table('users')
-        .insert({
-          discord_id: session.get('user').id,
-        })
-        .then(() => {
-          Database.from('users')
-            .where('userid', session.get('user').id)
-            .first()
-            .then((account) => {
-              session.put('account', account)
-            })
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    }
+    public async create({ response, session }) {
+        if (!session.get('account')) {
+            Database.table('users')
+                .insert({
+                    discord_id: session.get('user').id,
+                })
+                .then(() => {
+                    Database.from('users')
+                        .where('userid', session.get('user').id)
+                        .first()
+                        .then((account) => {
+                            session.put('account', account)
+                        })
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        }
 
-    return response.redirect('/')
-  }
+        return response.redirect('/')
+    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+import WithProps from 'App/Components/pages/WithProps'
+
+Route.get('/', 'TestReact.index')
+
+Route.get('/with-props', async ({ react }) => react.render(WithProps, { name: 'my friend' }))
+
+/*
 Route.get('/', async ({ view }) => {
-  return view.render('home')
+    return view.render('home')
 })
 
 Route.get('/dashboard', 'DashboardController.index').middleware('auth')
@@ -9,12 +16,12 @@ Route.get('/dashboard', 'DashboardController.index').middleware('auth')
 Route.get('/account/create', 'AccountController.create').middleware('auth')
 
 Route.get('/me', async ({ response, session }) => {
-  const id = session.get('user').id
-  return response.redirect(`/user/${id}`)
+    const id = session.get('user').id
+    return response.redirect(`/user/${id}`)
 })
 
 Route.get('/user/:id', async ({ view, params }) => {
-  return view.render('user', { id: params.id })
+    return view.render('user', { id: params.id })
 })
 
 Route.get('/login', 'AuthController.login')
@@ -22,3 +29,4 @@ Route.get('/login', 'AuthController.login')
 Route.get('/logout', 'AuthController.logout')
 
 Route.get('/discord/callback', 'AuthController.callback')
+*/
